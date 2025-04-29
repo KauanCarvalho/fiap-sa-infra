7383c0d8e975f59879afba569f4837c294072a89
Kauan Carvalho Calasans <kauan.carvalho.calasans@gmail.com>
2025-04-29 19:24:22 -0300

    feat: trigger pipeline

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -19,3 +19,28 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "eks_nodes_sg" {
+  name        = "eks-nodes-sg"
+  description = "Allow communication between EKS nodes"
+  vpc_id      = data.aws_vpc.vpc.id
+
+  ingress {
+    description = "Node-to-node"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-nodes-sg"
+  }
+}
