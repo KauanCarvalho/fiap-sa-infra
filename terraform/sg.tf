2dd0cafc6e25f11ae1d51a212e5a200de29a6ca3
Kauan Carvalho Calasans <kauan.carvalho.calasans@gmail.com>
2025-04-29 22:52:59 -0300

    feat: test

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -44,3 +44,14 @@ resource "aws_security_group" "eks_nodes_sg" {
     Name = "eks-nodes-sg"
   }
 }
+
+resource "aws_security_group_rule" "allow_mysql_from_eks" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg.id
+  source_security_group_id = aws_security_group.eks_nodes_sg.id
+
+  depends_on = [aws_security_group.rds_sg]
+}
