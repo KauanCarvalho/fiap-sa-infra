9c104567d1454aef7ee33b4369d54d333747db24
Kauan Carvalho Calasans <kauan.carvalho.calasans@gmail.com>
2025-05-04 18:20:52 -0300

    feat: adds ecr

--- production/terraform/outputs.tf
+++ production/terraform/outputs.tf
@@ -1,3 +1,5 @@
+# RDS.
+
 output "mysql_order_db_name" {
   value     = var.mysql_order_db_name
   sensitive = true
@@ -28,9 +30,13 @@ output "mysql_product_db_password" {
   sensitive = true
 }
 
+# SNS.
+
 output "sns_topic_arn" {
   value = aws_sns_topic.payment_events.arn
 }
+
+# SQS.
 
 output "sqs_queue_url" {
   value = aws_sqs_queue.order_queue.id
@@ -38,4 +44,14 @@ output "sqs_queue_url" {
 
 output "sqs_queue_arn" {
   value = aws_sqs_queue.order_queue.arn
+}
+
+# ECR.
+
+output "ecr_repository_name" {
+  value = aws_ecr_repository.fiap_sa_eks_ecr_repository.name
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.fiap_sa_eks_ecr_repository.repository_url
 }
